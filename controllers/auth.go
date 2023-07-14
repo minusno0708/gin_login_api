81012b3381792816774fafb4c68309cfb673aae5
minusno0708 <naoyao1014@gmail.com>
2023-07-14 22:17:56 +0900

    signup機能実装

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,25 +1,35 @@
 package controllers
 
 import (
-	"net/http"
+    "net/http"
 
-	"github.com/gin-gonic/gin"
+    "jwt-gin/models"
+
+    "github.com/gin-gonic/gin"
 )
 
 type RegisterInput struct {
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+    Username string `json:"username" binding:"required"`
+    Password string `json:"password" binding:"required"`
 }
 
 func Register(c *gin.Context) {
-	var input RegisterInput
+    var input RegisterInput
 
-	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+    if err := c.ShouldBindJSON(&input); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
 
-	c.JSON(http.StatusOK, gin.H{
-		"data": "validated!",
-	})
+    user := models.User{Username: input.Username, Password: input.Password}
+
+    user, err := user.Save()
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{
+        "data": user.PrepareOutput(),
+    })
 }
